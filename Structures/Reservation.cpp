fb7cb8a44c6d09cc966913da32f678049942972a
Catherine-H <titispeed@gmail.com>
2016-04-06 20:49:31 +0200

    Vérification de la date et de l'heure pour une réservation

--- Structures/Reservation.cpp
+++ Structures/Reservation.cpp
@@ -45,6 +45,7 @@ MesReservation::MesReservation()
  
 void MesReservation::ajouterReservation(){
 	    
+	    //initialisation des variables
 	    char DateTxT[11];
 	    char HeureTxT[6];
 		time_t date;
@@ -55,24 +56,24 @@ void MesReservation::ajouterReservation(){
 	    int hh;
 	    int min;
 	    
-	    
-	    
 	    int nbPersone;
 	    char nom[20];
-	      		
-	    printf("Date : (jj/mm/aaaa))");          
+	    
+	    
+	    int test=0;
+	    
+	while(test==0)	  		  	
+    {
+	    
+	   	
+	    printf("Entrer la date : (jj/mm/aaaa))");          
        	cin >> DateTxT;
        	
-		printf("heure : (hh:mm)");          
+		printf("Entrer l'heure (11h30-13h30 19h-21h) : (hh:mm)");          
        	cin >> HeureTxT;
-	    
-	    printf("nb de personnes : (int)");          
-       	cin >> nbPersone;
-	    
-	    printf("a quel nom ?(text)");          
-       	cin >> nom;
-	    
-		struct tm DateEtHeure;
+       	
+       	
+       	struct tm DateEtHeure;
 		
 		sscanf (DateTxT,"%d/%d/%d",&jj,&mm,&aaaa);
 		sscanf (HeureTxT,"%d:%d",&hh,&min);
@@ -83,10 +84,109 @@ void MesReservation::ajouterReservation(){
 		DateEtHeure.tm_hour = hh;
 		DateEtHeure.tm_min = min;	
 		DateEtHeure.tm_sec = 0;
+       	
+       date = mktime(&DateEtHeure);
+       	
+       	
+       	//variable concernant la date du jour (initialise après la saisie pour que les tests ne soit pas faussés)
+     	struct tm Today;
+		time_t maintenant;
+	
+		time(&maintenant);
+		Today = *localtime(&maintenant);
+	
+	
+
+	
+		int DateValide=0;
+   
+	    
+		//verification de la validité de la date
+       	if(mm>0 && mm<12 && aaaa>=Today.tm_year + 1900 && aaaa<=Today.tm_year + 1901){
+       			
+       		//si le jour est plus petit ou egal au nombre de jour dans le mois  (ici test pour mois à 31 jours) 
+			if((mm==1 || mm==3 || mm==5 || mm==7 || mm==8 || mm==10 || mm==12 )&& jj<=31) {
+				DateValide=1;				
+			} 
+			
+			//sinon test pour mois à 30 jours)
+			else if ((mm==4 || mm==6 || mm==9 || mm==11 )&& jj<=30) {
+				DateValide=1;
+			}
+				 
+			//pour le mois de février test si l'année est bisssextile ou non
+			else if(mm==2) {
+			
+				//si  bisssextile testpour fevrier à 29 jours
+				if(((aaaa % 4 == 0 && aaaa % 100 != 0) || aaaa % 400 == 0)&& jj<=29){
+					DateValide=1;
+				}
+				
+				//sinon test pour fevrier à 28 jours
+				 else if(jj==28){
+	           		DateValide=1;
+	        	}
+            }
+		}
+		
+		int DateFutur=0;
+		
+		//si la date est valide
+		if (DateValide==1) {
+			
+			//verifie si la date du jours n'est pas dépassé
+			//si année supérieur à l'année en cours c'est bon
+			if(aaaa>Today.tm_year + 1900){
+				DateFutur=1;
+			} 
+			
+			//sinon si l'année == année en cours
+			else if(aaaa==Today.tm_year +1900){
+				
+				//si la date n'est pas superieur de plus d'un mois de la date du jour, c'est bon
+				//(Today.tm_mon+1 == le mois actuel,Today.tm_mon+2==mois suivant)
+				if (mm==Today.tm_mon+2 && jj<=Today.tm_mday ) {	
+						DateFutur=1;	
+				}
+				
+				//pareil
+				else if(mm==Today.tm_mon+1) {	
+					if(jj>Today.tm_mday || hh>Today.tm_hour || hh==Today.tm_hour && min>=Today.tm_min){
+						DateFutur=1;		
+					}
+			
+				}
+			}
+		}
+		
+		//si date est dans le futur (donc valide est pas dépassé)
+		if(DateFutur==1){
+			//verifie si l'heure conrespond au horaire ou le client peut arriver (11h30-13h30 19h-21h)
+			//test pour les horaires du matin (11h30-13h30)  
+			if (hh>11 && hh<13 || hh==11 && min>=30 || hh==13 && min <30 )	{
+				test =1;	
+			}
+			//test pour les horaires du matin (19h-21h) 
+			else if( hh>=19 && hh<21 ||hh==21 && min==0 ){
+				test=1;
+			}
+									
+		}		
+    }    
+	
+       	
+	    
+	    printf("nb de personnes : (int)");          
+       	cin >> nbPersone;
+	    
+	    printf("a quel nom ?(text)");          
+       	cin >> nom;
+	    
+	
 		
 	    //printf ("%d/%d/%d %d:%d",DateEtHeure->tm_mday,DateEtHeure->tm_mon,DateEtHeure->tm_year,DateEtHeure->tm_hour,DateEtHeure->tm_min);
 	    
-	    date = mktime(&DateEtHeure);
+	    
 	    
 		importReservation(date, nbPersone, nom);
  	    
